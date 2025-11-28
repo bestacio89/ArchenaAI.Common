449b18be1c05c8db32b6b73eee432c5c716d6b60
Bernardo Estacio Abreu <bernardo.estacio89@gmail.com>
2025-11-28 10:34:12 +0100

    [Core] Add SkillRegistry, SkillContext and basic skill abstractions

---
+++ ArchenaAI.Common.Semantic.Kernel/ArchenaAI.Common.Semantic.Kernel/Skills/ISkillRegistry.cs
@@ -0,0 +1,9 @@
+﻿namespace ArchenaAI.Common.Semantic.Kernel.Skills
+{
+    public interface ISkillRegistry
+    {
+        void Register(IArchenaSkill skill);
+        IArchenaSkill? GetSkillAsync(string name);
+        IEnumerable<IArchenaSkill> GetAll();
+    }
+}
